893a4a56c5ae1f05732de75add6a71a37e885be5
Lapis! <87749912+LapisThe@users.noreply.github.com>
2026-04-04 02:14:01 -0700

    Fix return type

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,8 @@
 interface Module {
-	IsEven: (passedNumber: number) => boolean;
+	/**
+	 * @returns A boolean indicating whether or not the passed number is even, returning `undefined` and printing a warning if unsucessful
+	 */
+	IsEven: (passedNumber: number) => boolean | undefined;
 }
 
 declare const Module: Module;
